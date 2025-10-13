405aaf6d3c1facf7ed854fb2160d6fa7bf0da8a6
Ryan56h <dangbaoquockhanh3110@gmail.com>
2025-10-13 13:32:23 +0700

    Fix failing unit tests

--- UnitTest/UnitTest_Generated.cs
+++ UnitTest/UnitTest_Generated.cs
@@ -1,69 +1,62 @@
-﻿
-using System;
+﻿using System;
 using Xunit;
 
-namespace CalculatorTests
+
+namespace UnitTest
 {
-    /// <summary>
-    /// Unit tests for the <see cref="Calculator"/> class.
-    /// </summary>
-    public class CalculatorUnitTests
+    public class CalculatorTests
     {
-        private readonly Calculator _calc = new Calculator();
+        private readonly Calculator _calculator = new Calculator();
 
         [Theory]
-        [InlineData(1, 2, 3)]
-        [InlineData(-5, 7, 2)]
-        [InlineData(int.MaxValue, 0, int.MaxValue)]
-        public void Add_ReturnsCorrectSum(int a, int b, int expected)
+        [InlineData(5, 3, 8)]
+        [InlineData(-1, 1, 0)]
+        [InlineData(-5, -5, -10)]
+        [InlineData(0, 0, 0)]
+        public void Add_ShouldReturnCorrectSum(int a, int b, int expected)
         {
-            // Act
-            var result = _calc.Add(a, b);
-
-            // Assert
+            int result = _calculator.Add(a, b);
             Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData(5, 3, 2)]
-        [InlineData(-10, -5, -5)]
-        [InlineData(int.MinValue, 1, int.MinValue + 1)]
-        public void Sub_ReturnsCorrectDifference(int a, int b, int expected)
+        [InlineData(10, 4, 6)]
+        [InlineData(5, 5, 0)]
+        [InlineData(-5, 3, -8)]
+        [InlineData(3, -5, 8)]
+        public void Sub_ShouldReturnCorrectDifference(int a, int b, int expected)
         {
-            // Act
-            var result = _calc.Sub(a, b);
-
-            // Assert
+            int result = _calculator.Sub(a, b);
             Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData(4, 5, 20)]
-        [InlineData(-3, 6, -18)]
-        [InlineData(int.MaxValue / 2, 2, int.MaxValue)]   // avoid overflow in test data
-        public void Mul_ReturnsCorrectProduct(int a, int b, int expected)
+        [InlineData(3, 4, 12)]
+        [InlineData(-5, 6, -30)]
+        [InlineData(-7, -3, 21)]
+        [InlineData(10, 0, 0)]
+        public void Mul_ShouldReturnCorrectProduct(int a, int b, int expected)
         {
-            // Act
-            var result = _calc.Mul(a, b);
-
-            // Assert
+            int result = _calculator.Mul(a, b);
             Assert.Equal(expected, result);
         }
 
         [Theory]
         [InlineData(10, 2, 5)]
-        [InlineData(-9, -3, 3)]
-        [InlineData(int.MaxValue, 1, int.MaxValue)]
-        public void Div_ReturnsCorrectQuotient(int a, int b, int expected)
+        [InlineData(9, 3, 3)]
+        [InlineData(-15, 5, -3)]
+        [InlineData(0, 10, 0)]
+        public void Div_ShouldReturnCorrectQuotient(int a, int b, int expected)
         {
-            // Act
-            var result = _calc.Div(a, b);
-
-            // Assert
+            int result = _calculator.Div(a, b);
             Assert.Equal(expected, result);
         }
 
+        [Fact]
+        public void Div_ByZero_ShouldThrowDivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calculator.Div(10, 0));
+        }
     }
 }
-        //[Fact]
-        //public void Div_ByZero_ThrowsDivideByZero
+
